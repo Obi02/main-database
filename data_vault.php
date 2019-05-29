23bde5e39947e589ee43bde0128530de5d24bd21
Obi02 <51100843+Obi02@users.noreply.github.com>
2019-05-29 14:32:50 +1000

    Update data_vault.php

--- data_vault.php
+++ data_vault.php
@@ -3,6 +3,6 @@ $action = $_GET["action"];
 $myText = $_POST["mytext"];
 
 if($action = "save") {
-  $targetFolder = "/path/to/folder";
-  file_put_contents($targetFolder."mytext.txt", $myText);
+  $targetFolder = "/tree/master/src";
+  file_put_contents($targetFolder."storage.txt", $myText);
 }
