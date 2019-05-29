960e5a4e1dd81585163c7e5970fd5d53c294219e
Obi02 <51100843+Obi02@users.noreply.github.com>
2019-05-29 14:38:15 +1000

    Delete data_vault.php

--- data_vault.php
+++
@@ -1,8 +0,0 @@
-<?php
-$action = $_GET["action"];
-$myText = $_POST["mytext"];
-
-if($action = "save") {
-  $targetFolder = "/tree/master/src";
-  file_put_contents($targetFolder."storage.txt", $myText);
-}
