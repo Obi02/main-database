615b7dd16b02c17d41d891b15e7397715ccab167
Obi02 <51100843+Obi02@users.noreply.github.com>
2019-05-29 14:30:34 +1000

    Create data_vault.php

---
+++ data_vault.php
@@ -0,0 +1,8 @@
+<?php
+$action = $_GET["action"];
+$myText = $_POST["mytext"];
+
+if($action = "save") {
+  $targetFolder = "/path/to/folder";
+  file_put_contents($targetFolder."mytext.txt", $myText);
+}
